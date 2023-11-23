108ffdb6cbae8c7e2e7e7be710024b0d85048ea9
Dan <daniel.speranskiy@live.com>
2023-11-23 09:09:54 +0400

    Added custom styles for the theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import Wrapper from "@/components/Wrapper";
 
 
 export default function Home() {
   return (
-    <main className="">
-      <div className="text-3xl font-bold underline">Hello world!</div>
-    
-    </main>
+   <Wrapper>
+     <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1>Sail through time, shop the retro rhyme: Explore, Experience, Embrace at Retro Emporium - Where Nostalgia Meets Modern Elegance!</h1>
+     </div>
+   </Wrapper>
   )
 }
---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import { cn } from '@/lib/utils'
+import {ReactNode} from 'react'
+
+const Wrapper = ({ children , className }: { children: ReactNode, className?: string}) => {
+  return (
+    <div className={cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper
