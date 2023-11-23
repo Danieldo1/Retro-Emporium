0752b552768d10660caa44ac86971ebd75be08aa
Dan <daniel.speranskiy@live.com>
2023-11-23 17:06:23 +0400

    Adding backend configuration need to set up MongoDB

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import { getPayloadClient } from './getPayload'
+import { nextApp, nextHandler } from './next-utils'
+
+const app = express()
+
+const PORT = Number(process.env.PORT) || 3000
+
+const start = async () => {
+    
+    const payload = await getPayloadClient({
+        initOptions: {
+            express: app,
+            onInit: async (cms) => {
+                cms.logger.info(`Admin URL: ${cms.getAdminURL()}`)
+            },
+        },
+    })
+
+    app.use((req, res) => nextHandler(req, res))
+
+    nextApp.prepare().then(() => {
+        payload.logger.info(`Next.js started on port ${PORT}`)
+        app.listen(PORT, async () => {
+            payload.logger.info(`Admin URL: ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
+}
+
+start()
