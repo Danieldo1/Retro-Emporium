ad80bfe11e8085eefa884f2beabae17f382817a4
Dan <daniel.speranskiy@live.com>
2023-11-24 23:25:11 +0400

    Styles some element of product page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,7 +5,7 @@ import Wrapper from '@/components/Wrapper'
 import { PROD_CATEGORIES } from '@/config'
 import { getPayloadClient } from '@/getPayload'
 import { formatPrice } from '@/lib/utils'
-import { CheckIcon, ShieldCheck } from 'lucide-react'
+import { CheckIcon, ShieldCheck, Truck } from 'lucide-react'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import React from 'react'
@@ -50,9 +50,14 @@ const Page = async ({params}:PageProps) => {
   const label = PROD_CATEGORIES.find(({value})=> value === product.category)?.label
   const validURL = product.images.map(({image}) => (typeof image === 'string' ? image : image.url)).filter(Boolean) as string[]
   return (
-    <Wrapper className='bg-background'>
-      <div className="bg-background">
-        <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8'>
+    <>
+   <div className='flex-1 py-1 bg-foreground  flex justify-center align-middle items-center z-10 relative '>
+  <Truck  className='h-5 w-5 flex-shrink-0  text-background' />
+  <p className='ml-2 text-lg text1 text-background font-bold tracking-wider '>Free shipping on orders over €75</p>
+</div>
+    <Wrapper className='bg-background relative -pt-10 '>
+      <div className="bg-background ">
+        <div className='mx-auto max-w-2xl px-4 py-4 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-3 lg:px-3'>
 
           <div className='lg:max-w-lg lg:self-end '>
             <ol className='flex items-center space-x-2'>
@@ -97,11 +102,6 @@ const Page = async ({params}:PageProps) => {
                       <p className='text-base text-muted-foreground'>{product.description}</p>
                   </div>
 
-
-                  <div className='mt-6 flex items-center'>
-                        <CheckIcon className='h-5 w-5 flex-shrink-0  text-secondary' />
-                        <p className='ml-2 text-sm text-foreground'>Free shipping on orders over $75</p>
-                  </div>
             </section>
           </div>
 
@@ -133,6 +133,7 @@ const Page = async ({params}:PageProps) => {
 
       <ProductReel href='/products' query={{limit: 4, category: product.category}} title={`Similar ${label} Products`} subtitle={`Browse our collection of ${label} just like '${product.name}'`} />
     </Wrapper>
+    </>
   )
 }
 
